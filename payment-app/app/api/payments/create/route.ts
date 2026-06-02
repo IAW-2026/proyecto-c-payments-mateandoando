6f8a0afd71e927b0e6ee4152ca47622480b2dcbe
Agustin Ramirez Ramos <agustinramirez9560@gmail.com>
2026-06-01 22:54:55 -0300

    Mas arreglos

--- payment-app/app/api/payments/create/route.ts
+++ payment-app/app/api/payments/create/route.ts
@@ -3,6 +3,7 @@ import crypto from 'crypto'; // Librería nativa para generar UUIDs aleatorios
 import { prisma } from "../../../lib/prisma";
 //Importamos la libreria de Mercado Pago
 import {MercadoPagoConfig, Preference} from 'mercadopago';
+import { auth } from "@clerk/nextjs/server";
 
 //Le pasamos el token de .env para que sepa que soy yo
 const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN || '' });
@@ -17,6 +18,16 @@ interface CreatePaymentRequest {
 //Endpoint para que la Seller App cree una nueva transaccion de pago cuando se genere una orden de compra.
 export async function POST(request: NextRequest) {
   try {
+
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "No autorizado. Debes iniciar sesión para crear un pago." },
+        { status: 401 }
+      );
+    }
+    
     const body = (await request.json()) as CreatePaymentRequest; //Forzamos el tipado con la interface
     const {id_purchase_order, id_buyer, total_price} = body;
     
