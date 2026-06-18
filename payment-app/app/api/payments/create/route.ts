7fad8b67db5ec9993c41c2a103eb2f1cc38613f7
Agustin Ramirez Ramos <agustinramirez9560@gmail.com>
2026-06-17 23:34:41 -0300

    Arreglo

--- payment-app/app/api/payments/create/route.ts
+++ payment-app/app/api/payments/create/route.ts
@@ -48,6 +48,7 @@ export async function POST(request: NextRequest) {
 
     const nuevoPago = await prisma.payment_order.create({
       data: {
+        idPaymentOperation: id_operacion_pago,
         idPurchaseOrder: id_purchase_order,
         idBuyer: id_buyer,
         totalPrice: total_price,
